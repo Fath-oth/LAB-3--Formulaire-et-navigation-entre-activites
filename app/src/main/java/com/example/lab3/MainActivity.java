995a7142e588b77da9224d92f7e4dd2bda554cba
unknown <o.fath1142@uca.ac.ma>
2026-03-24 22:25:15 +0100

    LAB3

---
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.lab3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nom, email, phone, adresse, ville;
+
+    private Button btnEnvoyer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nom     = findViewById(R.id.name);
+        email   = findViewById(R.id.email);
+        phone   = findViewById(R.id.phone);
+        adresse = findViewById(R.id.adresse);
+        ville   = findViewById(R.id.ville);
+        btnEnvoyer = findViewById(R.id.send);
+
+        btnEnvoyer.setOnClickListener(v -> {
+            // 6.1) Lire le texte des champs
+            String sNom     = nom.getText().toString().trim();
+            String sEmail   = email.getText().toString().trim();
+            String sPhone   = phone.getText().toString().trim();
+            String sAdresse = adresse.getText().toString().trim();
+            String sVille   = ville.getText().toString().trim();
+
+            if (sNom.isEmpty() || sEmail.isEmpty()) {
+                Toast.makeText(this, "Nom et Email sont obligatoires.", Toast.LENGTH_SHORT).show();
+                return; // stoppe l'envoi
+            }
+
+            Intent i = new Intent(MainActivity.this, Screen2Activity.class);
+
+            i.putExtra("nom", sNom);
+            i.putExtra("email", sEmail);
+            i.putExtra("phone", sPhone);
+            i.putExtra("adresse", sAdresse);
+            i.putExtra("ville", sVille);
+
+            startActivity(i);
+        });
+    }
+}
